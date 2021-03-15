09d4e9a47de9ce461492905c68bb927073b372a2
kalsij <63124857+kalsij@users.noreply.github.com>
2021-03-15 14:01:53 -0400

    Update Contact Us.js

--- Javascript/Contact Us.js
+++ Javascript/Contact Us.js
@@ -4,6 +4,7 @@ function checkorder(){
     if (ordername.value.search(/^#{1}[0-9]{5}[A-Z]{2}$/)==-1)
          alert("Please enter a correct order number (Form: #12345AB)");
     else 
+        alert("Thank you, we wil get back to you within 3 days.")
     return true
     
 }
