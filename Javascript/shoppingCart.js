8cbe186f587028d2cf974f3707d620c42638b1c8
Aykchen <78436038+Aykchen@users.noreply.github.com>
2021-03-14 20:22:40 -0400

    Add files via upload

---
+++ Javascript/shoppingCart.js
@@ -0,0 +1,94 @@
+var incrementButtonShoppingCart = document.getElementsByClassName('incrementButton');
+var decrementButtonShoppingCart = document.getElementsByClassName('decrementButton');
+
+//increment button
+for(var i = 0; i < incrementButtonShoppingCart.length; i++) {
+    var button = incrementButtonShoppingCart[i];
+    button.addEventListener('click', function(event){
+      var buttonClicked = event.target;
+
+      var input = buttonClicked.parentElement.children[1].innerText;
+
+      buttonClicked.parentElement.children[1].innerText = parseInt(input) + 1;
+
+      updateTotalQuantityShoppingCart();
+      updateTotalPriceShoppingCart();
+    })
+}
+//decrement button
+for(var i = 0; i < decrementButtonShoppingCart.length; i++) {
+    var button = decrementButtonShoppingCart[i];
+    button.addEventListener('click', function(event){
+      var buttonClicked = event.target;
+
+      var input = buttonClicked.parentElement.children[1].innerText;
+    
+      if(input != '0'){
+        buttonClicked.parentElement.children[1].innerText = parseInt(input) - 1;
+        updateTotalQuantityShoppingCart();
+        updateTotalPriceShoppingCart();
+      }
+    })
+}
+
+//Remove items
+var removeItem = document.getElementsByClassName("delete-trash")
+
+for (var i = 0; i < removeItem.length; i++) {
+    var button = removeItem[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateTotalQuantityShoppingCart()
+        updateTotalPriceShoppingCart()
+        
+    })
+}
+
+//Update total quantity
+function updateTotalQuantityShoppingCart() {
+    var cartItemContainer = document.getElementsByClassName('Cart')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('listMyCart')
+    var quantity = 0
+    
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var quantityElement = parseInt(cartRow.getElementsByClassName('quantityProduct')[0].innerText)
+
+        quantity = quantity + quantityElement;
+
+    }
+    
+    document.getElementsByClassName("totalItems")[0].innerText = quantity 
+}
+
+//Update total prices
+function updateTotalPriceShoppingCart() {
+    var cartItemContainer = document.getElementsByClassName('Cart')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('listMyCart')
+    var subTotal = 0
+    var total = 0
+    var GSTtaxe = 0
+    var QSTtaxe = 0
+
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('priceItem')[0].innerText
+        var quantityElement = parseInt(cartRow.getElementsByClassName('quantityProduct')[0].innerText)
+
+        var price = parseFloat(priceElement.replace('$',''))
+        console.log(price);
+        subTotal = subTotal + (price*quantityElement)
+
+    }
+
+    GSTtaxe = (0.05*subTotal)
+    QSTtaxe = (0.09975*subTotal)
+
+    total = (subTotal+GSTtaxe+QSTtaxe)
+ 
+    document.getElementsByClassName("subtotalPrice")[0].innerText = '$ ' + Number(Math.round(subTotal+'e2')+'e-2')
+    document.getElementsByClassName("GSTPrice")[0].innerText = '$ ' + Number(Math.round(GSTtaxe+'e2')+'e-2')
+    document.getElementsByClassName("QSTPrice")[0].innerText = '$ ' + Number(Math.round(QSTtaxe+'e2')+'e-2')
+    document.getElementsByClassName("totalPrice")[0].innerText = '$ ' + Number(Math.round(total+'e2')+'e-2')
+}
