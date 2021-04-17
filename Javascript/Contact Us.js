2975eb050d66b31883355a488f21eaf584e63668
kalsij <63124857+kalsij@users.noreply.github.com>
2021-04-17 03:34:50 -0400

    Add files via upload

--- Javascript/Contact Us.js
+++ Javascript/Contact Us.js
@@ -1,8 +1,10 @@
 //Contact Page
 function checkorder(){
     var ordername = document.getElementById('ordernumber');
-    if (ordername.value.search(/^#{1}[0-9]{5}[A-Z]{2}$/)==-1)
+    if (ordername.value.search(/^#{1}[0-9]{5}[A-Z]{2}$/)==-1){
          alert("Please enter a correct order number (Form: #12345AB)");
+         return false;
+    }
     else 
     return true;
     
@@ -10,8 +12,10 @@ function checkorder(){
 
 function checkusername(){
     var username = document.getElementById('username');
-    if (username.value.search(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)==-1)
-         alert("Please enter a correct user name(email)");
+    if (username.value.search(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)==-1){
+         alert("Please enter a correct user name (email)");
+         return false;
+    }
     else
     return true;
 }
@@ -19,5 +23,7 @@ function checkusername(){
 function checked(){
     if(checkusername() && checkorder() == 1){
         alert("Thank you, we wil get back to you within 3 days.")
+        return true;
     }
+    else return false;
 }
