58677320bf295371b909151f7a452784754725e1
georgia1515 <77301007+georgia1515@users.noreply.github.com>
2021-04-18 18:10:29 -0400

    Update product-detail.php

--- PHP/product-detail.php
+++ PHP/product-detail.php
@@ -1,29 +1,61 @@
-<?php
-session_start();
-session_unset();
+<?php 
+        session_start();
+        if(empty($_POST[logout])){
+
+        }
+        else{
+        session_unset();
+        }
+        if(empty($_SESSION[fullName])){
+        $sign1 = "<li><a href=\"SignUp.php\">Sign up</a></li>";
+        $sign2 = "<li><a href=\"SignIn.php\">Sign in</a></li>";
+        }
+        else{
+            $sign1 = "<li><a href=\"\">".$_SESSION[fullName]."</a></li>";
+            $sign2 = "<li><div style=\"text-align:center;\"><form method=\"post\"><input type=\"submit\" value=\"Log Out\" style=\"outline:none;border:none;background-color:Transparent;color:rgb(226, 215, 215);font-size:20px;\" name=\"logout\"/></form></div></li>";
+        }
+
+
+
+        $id = $_REQUEST["id"];
+        $myfile = fopen("../Data/productList.txt", "r") or die("Unable to open file!");
+                                    
+        while(!feof($myfile)) {
+            $line = fgets($myfile);
+            if (strlen($line) < 5){
+            break;
+            }
+
+            $elements = explode(";", $line);
+            if (strcmp($id, $elements[0]) == 0) {
+                  $title=$elements[2];
+            }
+        }                         
+        fclose($myfile);
+       
+        $js_file = "../Javascript/script.js";
+
+      //  include("header.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>Coca-cola</title>
+    <title>Product</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
- 
+    //Bootstrap
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
- 
+    //Style
     <link rel="stylesheet" type="text/css" href="../Styles/newStyle.css" />
-    
 </head>
 
     <body>
-        
-
         <!--nav bar-->
         <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top" style="background-color: blue;">
             <h1 style="margin-top:0mm;color:white; font-size:50px;font-style:italic; font-weight:bold; margin-right:1cm;">
@@ -39,15 +71,14 @@ session_unset();
 
                     <li><a href="#">Aisles</a>
                         <ul>
-                            <li><a href="Fruits and vegetables.html">Fruits and Vegetables</a></li>
-                            <li><a href="Meat.html">Meat</a></li>
-                            <li><a href="Dairy.html">Dairy</a></li>
-                            <li><a href="Bread.html">Bread</a></li>
-                            <li><a href="drinks.php">Drinks</a></li>
-                            <li><a href="pasta.html">Pasta</a></li>
-                            </a>
-
-
+                        <li><a href="Fruits and vegetables.php">Fruits and Vegetables</a></li>
+                        <li><a href="Meat.php">Meat</a></li>
+                        <li><a href="Dairy.php">Dairy</a></li>
+                        <li><a href="Bread.php">Bread</a></li>
+                        <li><a href="drinks.php">Drinks</a></li>
+                        <li><a href="pasta.php">Pasta</a></li>
+                   
+                        </a>
                     </li>
 
                 </ul>
@@ -62,14 +93,14 @@ session_unset();
 
             <a href="ShoppingCart.html"><img src="Media/cart_logo.png" alt="Shopping Cart" width="50px" height="50px"
                     style="float:right"></a>
-        </nav>
+    </nav>
         <!--product description sheet-->
         <h1> </h1>
         <div class="container shadow-lg " >
             
             <?php
 
-            $id = $_REQUEST["id"];
+                $id = $_REQUEST["id"];
                 $myfile = fopen("../Data/productList.txt", "r") or die("Unable to open file!");
                                
                 while(!feof($myfile)) {
@@ -77,62 +108,48 @@ session_unset();
                     if (strlen($line) < 5){
                         break;
                     }
-                        
+                            
                     $elements = explode(";", $line);
-
-                    //goes through every line -> check if the first element in the line which is the index id -> corresponds with the $id passed -> if same # = print it
                     if (strcmp($id, $elements[0]) == 0) {
                         //echo "Id found" . $elements[3];
 
 
                         echo "<div class=\"row\">";
 
-             
-                        echo "<div class=\"col-md-6 \" >";
-                            echo"<img class=\"picture\" src=\"../$elements[1]\" alt= \" $elements[2] \" style=\"width:90%; display: block; margin: 0 auto; margin-bottom: 20px;\">";
-                        echo "</div>";
-        
-                        echo "<div class=\" col-md-5 \" style = \" margin-right: 50px\">";
-                            echo "<h4 ><b>$elements[2]</b></h4>";
-                                echo "<br/>";
-                                
-                                echo "<h3  style=\"color:red\">";
-                                    echo "CAD 1.74$/unit";
-                                echo "</h3>";
-                                echo "<p>";
-                                    echo "(355 mL)";
-                                echo "</p>";
-
-                                echo "
-                                <form action = \"ShoppingCart.php\" method = \"POST\">
-                                <input class = \"product-quantity ml-2 mr-2\" min = \"1\"type = \"number\" name = \"quantity\" id=\"totalMacaroni\" value=\"1\" style=\"width:20%;\">
-                              
-                                <!-- <button  onclick=\"incrementCola()\">+</button>
-                                <p id=\"quantity\" style=\"display: inline;\" >1</p>
-                                <button  onclick=\"decrementCola()\">-</button> -->
-                                
-                                <div class=\"btn\">
-                                    <input type=\"submit\" onclick=\"addLemonCart()\" value=\"Add to cart\" style = \"background-color: black; color : white\">
-                                </div>
-                                <div>
-                                    <p id=\"addedCola\"></p>
-                                    <p id = \"totalCola\"></p>
-                                </div>
-                              
-                                </form>";
-
-                                echo "<div style=\"padding-top: 20px;\">";
-                                    echo "<button class=\"dropbtn\" onclick=\"showColaDescription()\">Product Description</button>";
-                                    echo "<p id=\"colaDescription\" style=\"visibility:hidden;\">$elements[7]</p>";
-                                echo "</div>";
-                                // <!-- JavaScript -->
-                                echo "<script type = \"text/javascript\" src=\"../Javascript/script.js\"></script>";
-                        echo "</div>";
-                    echo "</div>"; 
-                        
                     
-                        //pass elements in product page to shopping cart or other pages
-                        $_SESSION["elementsProduct"] = $elements;
+                            echo "<div class=\"col-md-6 \" >";
+                                echo"<img class=\"picture\" src=\"../$elements[1]\" alt= \" $elements[2] \" style=\"width:90%; display: block; margin: 0 auto; margin-bottom: 20px;\">";
+                            echo "</div>";
+                
+                            echo "<div class=\" col-md-5 \" style = \" margin-right: 50px\">";
+                                echo "<h4 ><b>$elements[2]</b></h4>";
+                                     echo "<br/>";
+                                        
+                                    echo "<h3  style=\"color:red\">";
+                                        echo "CAD $elements[6]";
+                                    echo "</h3>";
+                                    echo "<p>";
+                                        echo "($elements[8])";
+                                    echo "</p>";
+                                    echo "<button  type=\"button\" onclick=\"incrementCola()\">+</button>";
+                                    echo "<span id=\"numOfCola\">1</span>";   
+                                    echo "<button type=\"button\" onclick=\"decrementCola()\">-</button>";
+                            
+                                    echo "<div class=\"btn\">";
+                                    echo "<button type=\"submit\" onclick=\"addColaToCart()\">Add to cart</button>";
+                                    echo "</div>";
+                                    echo "<br/>";
+                                    echo "<p id=\"addedCola\"></p>";
+                                     echo "<p id=\"totalCola\"></p>";
+                                    echo "<div style=\"padding-top: 20px;\">";
+                                        echo "<button class=\"dropbtn\" onclick=\"showColaDescription()\">Product Description</button>";
+                                        echo "<p id=\"colaDescription\" style=\"visibility:hidden;\">$elements[7]</p>";
+                                        echo "</div>";
+                                        // <!-- JavaScript -->
+                                    echo "<script type = \"text/javascript\" src=\"../Javascript/script.js\"></script>";
+                            echo "</div>";
+                        echo "</div>"; 
+
                     }
                 }
 
@@ -147,8 +164,13 @@ session_unset();
         <h1>Other related items</h1>
         <div class=" justify-content-md-center row">
                             <?php 
+                                $id = $_REQUEST["id"];
+
+
                                 $myfile = fopen("../Data/productList.txt", "r") or die("Unable to open file!");
                                 // Output one line until end-of-file
+                                $products= array();
+                                $category= "";
                                 while(!feof($myfile)) {
                                   $line = fgets($myfile);
                                   if (strlen($line) < 5){
@@ -156,31 +178,36 @@ session_unset();
                                   }
                             
                                   $elements = explode(";", $line);
-                                  
-                                  if (strcmp($elements[3], "Drinks") == 0)  {
+
+                                  if (strcmp($id, $elements[0]) == 0) {
+                                    $category=$elements[3];
+                                  }
+
+                                  array_push($products, $elements);
+                                }
+                                 fclose($myfile);
+
+                                foreach($products as $elements){
+
+                                  if (strcmp($id, $elements[0]) != 0 && strcmp($elements[3], $category) == 0) {
                                     echo "<div class=\"col-md-auto\">";
                                     echo "<div class=\"coca-cola\">";
                                         echo "<a href=\"product-detail.php?id=$elements[0]\">";
                                             echo "<img src=\"../$elements[1]\" alt= \" $elements[2] \" style=\"width:100% ; max-height:200px;\">";
                                         echo "</a>";
                                         echo "<h1 style=\"margin-top: auto; color:rgb(75, 75, 75);\">$elements[2]</h1>";
-                                        echo "<p class=\"price\" style=\"color:rgb(75, 75, 75);\">$elements[6]<br/>(355 mL)</p>";
+                                        echo "<p class=\"price\" style=\"color:rgb(75, 75, 75);\">$elements[6]<br/>($elements[8])</p>";
                                         echo "<p><button style=\"color:white;\">Add to Cart</button></p>";
                                     echo "</div>";
                                   echo "</div>";
-
-
                                   }  
-
                                 }
-                                fclose($myfile);
+                                
                             ?>
-        
-        
-        
         </div>
-          <!--footer-->      
-        <footer class="container py-4">
+
+
+<footer class="container py-4">
             <div class="row">
               <div class="col-12 col-md">
                 <small class="d-block mb-3 text-muted">&copy; 2021-2022 </br> </small>
@@ -215,8 +242,10 @@ session_unset();
                 </ul>
               </div>
             </div>
-          </footer>   
-       
+          </footer>  
+
+
     </body>
 
 </html>
+
