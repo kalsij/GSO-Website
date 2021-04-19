8f1b30d033bdc4c35becc463d1f5d49fbca8f2b1
fatema-a <78175202+fatema-a@users.noreply.github.com>
2021-04-19 17:35:05 -0400

    Update BackstoreUserList.php

--- PHP/BackstoreUserList.php
+++ PHP/BackstoreUserList.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -45,8 +44,8 @@
                     <h2 style="font-style: italic;">User List</h2>
                     
                     <!-- Add Product button -->
-                    <a href="../BackstoreEditOrderProfile.html" class="btn btn-primary" style="float: right;margin-bottom: 5px;">Add user</a>
-                    <td><a href='../BackstoreEditOrderProfile' class='btn btn-primary'style="float: right;margin-right: 5px;">Edit</a></td>
+                    <a href="BackstoreAddUser.php" class="btn btn-primary" style="float: right;margin-bottom: 5px;">Add user</a>
+                    <td><a href='BackstoreEditUser.php' class='btn btn-primary'style="float: right;margin-right: 5px;">Edit</a></td>
             
                     <!-- Order List -->
                     <?php function newRow($firstName, $lastName, $gender, $postalCode, $email, $password)
