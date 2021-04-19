63e6c808b7334af2a012f0093560bd1ac636a2b0
fatema-a <78175202+fatema-a@users.noreply.github.com>
2021-04-19 17:27:55 -0400

    Update BackstoreUserList.php

--- PHP/BackstoreUserList.php
+++ PHP/BackstoreUserList.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,116 +7,95 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!----bootstrap--->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/Javascript/script.js"></script>
-    <!----Style--->
-    <link rel="stylesheet" type="text/css" href="../Styles/BackstoreStyle.css" />
-
+    
+    <link rel="stylesheet" type="text/css" href="../Styles/BackstoreStyle.css"/>
 
 </head>
 
 <body>
-
     <!-- Nav Bar -->
     <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top" style="background-color: blue; position:fixed;">
-        <h1 style="margin-top:0mm;color:white; font-size:50px;font-style:italic; font-weight:bold; margin-right:1cm;">
-            GSO</h1>
+            <h1 style="margin-top:0mm;color:white; font-size:50px;font-style:italic; font-weight:bold; margin-right:1cm;">
+                GSO</h1>
     </nav>
 
-
-    <div class="container-fluid text-center">
+    
+    <div class="container-fluid text-center">    
         <div class="row content">
 
             <!-- Side Bar -->
-            <div class="col-sm-2 sidenav">
-                <br />
-                <p><a href="GroceryStore-1.php">Grocery Store</a></p>
-                <p><a href="BackstoreProductList.php">Product List</a></p>
-                <p><a href="BackstoreOrderList.php">Order List</a></p>
-                <p> <a href="BackstoreUserList.php">User List</a></p>
-                <p style="font-weight:bold;"><a href="GroceryStore-1.php">Log Out</a></p>
+       <div class="col-sm-2 sidenav">
+            <br/>
+            <p><a href="../GroceryStore-1.php">Grocery Store</a></p>
+            <p><a href="BackstoreProductList.php">Product List</a></p>
+            <p><a href="BackstoreOrderList.php"></a>Order List</p>
+            <p> <a href="BackstoreUserList.php">User List</a></p>
+            <p style="font-weight:bold;"><a href="../GroceryStore-1.php">Log Out</a></p>
             </div>
 
 
-            <div class="col-sm-8 text-left">
+            <div class="col-sm-8 text-left">       
                 <div class="right">
                     <h2 style="font-style: italic;">User List</h2>
-<br/>
-                   
-
+                    
+                    <!-- Add Product button -->
+                    <a href="../BackstoreEditOrderProfile.html" class="btn btn-primary" style="float: right;margin-bottom: 5px;">Add user</a>
+                    <td><a href='../BackstoreEditOrderProfile' class='btn btn-primary'style="float: right;margin-right: 5px;">Edit</a></td>
+            
+                    <!-- Order List -->
                     <?php function newRow($firstName, $lastName, $gender, $postalCode, $email, $password)
                     {
                         return
-                            "<tr>  
-                            <td><input name = 'checkbox' type='radio'></td>                                  
+                            "<tr>
+                            <td><input name = 'checkbox' value = '$firstName' type='radio'></td>             
                             <td>" . $firstName . "</td>
                             <td>" . $lastName . "</td>
                             <td>" . $gender . "</td>
                             <td>" . $postalCode . "</td>
-
                             <td>" . $email . "</td>
-
                             <td>" . $password . "</td>
-                         
-                            <td><input type ='button' value = 'Delete' class='btn btn-primary' onclick='deleteUser(this)' style='float:right;'/></td>
-
                             </tr>";
                     }
                     ?>
-
-                    <!-- User  List -->
-
-                    <?php
+                        
+                        <?php
                     echo "
-                    <form>
-                    <table class='table' id='productTable'>
-                    <thead id='productHead'>
+                    <form method = 'POST' action='deleteUser.php'>
+                    <table class=\"table\" id=\"UserTable\">
+                            <thead id=\"UserHead\">
                                 <tr>
-                                <th id=Order><p>Checkboxes</p></th>
-                                    <th class='tab'>First Name</p></th>
-                                    <th>Last Name</p></th>
-                                    <th>Gender</p></th>
-                                    <th>Postal Code</p></th>
-                                    <th>Email</p></th>
-                                    <th>Password</p></th> 
-                                    
-                                    </tr> 
-                                    </thead>
-                                    </form>
-                                    ";
-
+                                <th id=firstName><p>Checkboxes</p></th>
+                                <th id=firstName><p>First Name</p></th>
+                                <th id=lastName><p>Last Name</p></th>
+                                <th id=gender><p>Gender</p></th>
+                                <th id=postalCode><p>Postal Code</p></th>
+                                <th id=Email><p>Email</p></th>
+                                <th id=password><p>Password</p></th>
+                                    </tr> </thead>";
                     $file = fopen("../Data/users.txt", "r");
                     if ($file) {
                         while (($line = fgets($file)) !== false) {
                             $array = explode(" ", $line);
-                            echo newRow($array[0], $array[1], $array[2], $array[3], $array[4], $array[5]);
+                            echo newRow($array[0], $array[1], $array[2], $array[3], $array[4],$array[5]);
                         }
                     } else {
                         echo "The file doesnt Exist";
                     }
-                    echo "</table> 
-                    <br/>
-                    <a href='BackstoreAddUser.php' class='btn btn-primary' style='float: right;margin-bottom: 5px;'>Add User</a>
-                    <a href='BackstoreEditUser.php' class='btn btn-primary' style='float: right;margin-right: 5px;'> Edit</a>
-                    
-                    <script type='text/javascript' src='../Javascript/script.js'></script>"
+                    echo "</table>
+                    <button class='btn btn-primary' type='submit' value='1' name = 'DeleteUser' onclick='deleteUser()'>Delete</button>
+                    <script type='text/javascript' src='Javascript/userlist.js'></script>
+                    </form>"
+               
                     ?>
-                    <br/>
-                    <br/>
-                    <br/>
-                
-                    <br/>
-                   <!--footer-->
- <?php include("footer.php") ?>
-                </div>
-                 
-            </div>
-            
+                    <!-- Footer -->
+<?php include("footer.php") ?>
+        </div>
+    </div>
 </body>
-
-</html>
-
+</html> 
