de29a6957b08f7f3b8a41883754c51e3d6b8d831
georgia1515 <77301007+georgia1515@users.noreply.github.com>
2021-04-19 13:16:33 -0400

    Update Meat.php

--- PHP/Meat.php
+++ PHP/Meat.php
@@ -1,56 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+session_start();
+if(empty($_POST[logout])){
 
-<head>
-    <title>Meat</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!----------Bootstrap---------->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <!----------Style---------->
-    <link rel="stylesheet" type="text/css" href="../Styles/newStyle.css" />
-</head>
-
-<body>
-   <!----------nav bar---------->
-  <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top" style="background-color: blue;">
-    <h1 style="margin-top:0mm;color:white; font-size:50px;font-style:italic; font-weight:bold; margin-right:1cm;">
-        GSO</h1>
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-            <li><a href="GroceryStore-1.html">Home</a></li>
-
-            <li><a href="#">Aisles</a>
-                <ul>
-                <li><a href="Fruits and vegetables.php">Fruits and Vegetables</a></li>
-                        <li><a href="Meat.php">Meat</a></li>
-                        <li><a href="Dairy.php">Dairy</a></li>
-                        <li><a href="Bread.php">Bread</a></li>
-                        <li><a href="drinks.php">Drinks</a></li>
-                        <li><a href="pasta.php">Pasta</a></li>
-                    </li>
-                  </ul>
-
-        <li><a href="#">Account</a>
-            <ul>
-                <li><a href="SignUp.php">Sign up</a></li>
-                <li><a href="SignIn.php">Sign in</a></li>
-            </ul>
-        </ul>
-    </div>
-
-    <a href="ShoppingCart.php"><img src="../Media/cart_logo.png" alt="Shopping Cart" width="50px" height="50px"
-            style="float:right"></a>
-
-</nav>
+}
+else{
+session_unset();
+}
+if(empty($_SESSION[fullName])){
+$sign1 = "<li><a href=\"SignUp.php\">Sign up</a></li>";
+$sign2 = "<li><a href=\"SignIn.php\">Sign in</a></li>";
+}
+else{
+    $sign1 = "<li><a href=\"\">".$_SESSION[fullName]."</a></li>";
+    $sign2 = "<li><div style=\"text-align:center;\"><form method=\"post\"><input type=\"submit\" value=\"Log Out\" style=\"outline:none;border:none;background-color:Transparent;color:rgb(226, 215, 215);font-size:20px;\" name=\"logout\"/></form></div></li>";
+}
+$js_file = "../Javascript/script.js";
+$title = "Meat";
+include("header.php");
+?>
 <!----------Product Cards---------->
                 <h1 class="ProductTitle">Meat</h1>
                 <div class=" justify-content-md-center row">
