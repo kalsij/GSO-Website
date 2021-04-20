fa8930afb00dfa372584250d6f3bd84513437f28
fatema-a <78175202+fatema-a@users.noreply.github.com>
2021-04-20 01:17:20 -0400

    Update BackstoreAddUser.php

--- PHP/BackstoreAddUser.php
+++ PHP/BackstoreAddUser.php
@@ -1,4 +1,5 @@
- <!DOCTYPE html>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -6,7 +7,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    //Bootstrap
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -14,10 +14,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     
-    //Style
     <link rel="stylesheet" type="text/css" href="../Styles/BackstoreStyle.css"/>
-    //JavaScript
     <script type="text/javascript" src="../Javascript/userAdd.js"></script>
+    
 
 </head>
 
@@ -27,106 +26,173 @@
             <h1 style="margin-top:0mm;color:white; font-size:50px;font-style:italic; font-weight:bold; margin-right:1cm;">
                 GSO</h1>
     </nav>
-     
+
     
     <div class="container-fluid text-center">    
-        <div class="row content" >
+        <div class="row content">
 
             <!-- Side Bar -->
-            <div class="col-sm-2 sidenav">
-                <br/>
-                <p><a href="GroceryStore-1.html">Home</a></p>
-                <p><a href="GroceryStore-1.html">Grocery Store</a></p>
-                <p><a href="BackstoreProductList.html">Product List</a></p>
-                <p><a href="BackstoreOrderList.html">Order List</a></p>
-                <p> <a href="BackstoreUserList.html">User List</a></p>
-                <p style="font-weight:bold;"><a href="">Log Out</a></p>
+       <div class="col-sm-2 sidenav">
+            <br/>
+            <p><a href="GroceryStore-1.php">Grocery Store</a></p>
+            <p><a href="BackstoreProductList.php">Product List</a></p>
+            <p><a href="BackstoreOrderList.php">Order List</a></p>
+            <p> <a href="BackstoreUserList.php">User List</a></p>
+            <p style="font-weight:bold;"><a href="GroceryStore-1.php">Log Out</a></p>
             </div>
 
 
             <div class="col-sm-8 text-left">       
                 <div class="right">
+                    
                     <h2 style="font-style: italic;">Add User</h2>
                     <br/>
-    <form action="">
-              <div>
-            <input type="submit" name="signupSubmit" value="Add User" onclick="userAdd();return false;"class="btn btn-primary" style="float:right;"/>
-            <input type="submit" name="signupSubmit" value="Reset" onclick="clear()"class="btn btn-secondary" style="float:right; margin-right: 5px;"/>
+                    
+        <?php
+//read the file and find the correct line of data from the checkbox and store the information in an array for display
+            $userSelected = $_REQUEST["index"];
+            echo "<form name='addForm' id='formOrderEdit' class='orderFrom' action='adduser.php' method='POST'>";
 
-        </div>
-        <fieldset>
-            <br />
-            <div class="form-row">
-                        <div class="form-group col-md-6">
-            <div id="aAnnounce"></div>
-            <label>First Name:</label>
-            <input type="text" name="fName" class="form-control"id="firstName"   />
-            <span id="aFName"></span>
-            </div>
-            <br />
-            <div class="form-group col-md-6">
-            <label>Last Name:</label>
-            <br />
-            <input type="text" name="lName" class="form-control" id="lastName"   />
-            <span id="aLName"></span>
-            </div>
-            </div>
-            <br />
-            <label>Gender:</label>
-            <input type="radio" id="male" name="gender" style="margin: 0 10px 0 2px;" value="Male" >Male</input>
-            <input type="radio" id="female" name="gender" style="margin: 0 10px 0 30px;"value="Female" >Female</input>
-            <span id="aGender"></span>
-          <br />
-            <div class="form-row">
-                          
-                          <div class="form-group col-md-6">
-            <label>Postal Code:</label>
-            <input type="text" id="postalCode" class="form-control" />
-            <span id="aPost"></span>
-                          </div>
-                          </div>
+            $myfile = fopen("../Data/users.txt", "r") or die("Unable to open file!");
+            $lineNumber = 1;
 
-                          <div class="form-row">
-                          
-                          <div class="form-group col-md-6">
-            <label>Email:</label>
-           
-            <input type="text" name="email"class="form-control"  id="email1" />
-            <span id="aEmail1"></span>
-            <br /> </div>
-           <div class="form-group col-md-6">
-            <label>Confirm Email:</label>
-            
-            <input type="text" class="form-control" id="email2" />
-            <span id="aEmail2"></span>
-           </div>
-           </div>
-            <br />
-            <div class="form-row">
-                          
-                          <div class="form-group col-md-6">
-            <label>Password:</label>
-            
-            <input type="password" name="password"class="form-control" id="password1" placeholder="Atleast 8 Characters" />
-            <span id="aPass1"></span> 
-            </div>
-                          <div class="form-group col-md-6">
-            <label>Confirm Password:</label>
-            <input type="password" class="form-control" id="password2"  />
-            <span id="aPass2"></span>
-                          </div>
-                          </div>
+
+            while(!feof($myfile)) {
+                $line = fgets($myfile); 
+     
+                $lineArray =  explode(" ", $line);
+  
+
+    
+        
+
+            }
+
+
+            $firstName=$lineArray[0];
+            $lastName=$lineArray[1];
+            $gender=$lineArray[2];
+            $postal=$lineArray[3];
+            $email=$lineArray[4];
+            $password=$lineArray[5];
+  
+  
+  
+            fclose($myfile);
+    
+                ?>
+          
+                     <?php $userSelected = $_REQUEST["index"];
+                    
+                    echo" <form id=\"myform\" action=\"addUser.php?index=$userSelected\" method=\"post\">"?>
+                       <input type = "submit" value = "Save"  onsubmit="return userAdd() " class="btn btn-primary" style="float:right;"/>
+                       <input type = "reset" value = "Cancel" class="btn btn-secondary" style="float:right; margin-right: 5px;"/>
+                       <br/>  
+                       <!-- text emplacement for the errors of each input-->
+                       <p id="correctionFirst"></p>
+                        <p id="correctionLast"></p>
+                        <p id="correctionPostal"></p>
+                        <p id="correctionEmail"></p>
+                        <p id="correctionPassword"></p>
+                        <p id="correct"></p>
+
+                         <div class="form-row">
+                            
+                           <div class="form-group col-md-6">
+                             <label for="firstName">First Name</label>
+                             <input id="firstname" name="firstName"type="text" value="<?php echo $firstName ?>" class="form-control">
+                           </div>
+                           <div class="form-group col-md-6">
+                               <label for="lastName">Last Name</label>
+                               <input id="lastname" name="lastName"type="text" value="<?php echo $lastName?>" class="form-control">
+                           </div> 
+
+
+                           <?php
+                             echo"<div>";
+                             echo"<label for=\"gender\">Gender</label><br/>";
+                             echo"<label><input type=\"radio\" name=\"gender\" id=\"female\" style=\"margin: 0 10px 0 2px;\" value=\"Female\"";
+                             if(strcmp($gender, "Female") == 0){
+                                              echo "checked";
+                                       }
+                                       echo"/>Female</label>"; 
+                                       echo"<label><input type=\"radio\" name=\"gender\" id=\"male\" style=\"margin: 0 10px 0 2px;\" value=\"Male\"";
+                                       if(strcmp($gender, "Male") == 0){
+                                                        echo "checked";
+                                                 }
+                                                 echo"/>Male</label>";
+                           echo"</div>"; 
+
+                           ?>
+                      </div>
  
-        </fieldset>
+ 
+                   <div class="form-row">
+                     
+                     <div class="form-group col-md-6">
+                       <label name="postalCode">Postal Code</label>
+                       <input type="text" name="postal" id="postCode" value="<?php echo $postal?>" class="form-control">
+                     </div>
+                   </div>
+ 
+                   <div class="form-row">
+                     
+                       <div class="form-group col-md-6">
+                         <label name="email">Email</label>
+                         <input type="text" name="email" id="emailAddress" value="<?php echo $email?>" class="form-control">
+                       </div>
+                       
+                   </div>
+   
+                   <div class="form-row">
+                     
+                       <div class="form-group col-md-6">
+                         <label name="password">Password</label>
+                         <input type="text" id="password" name="password" value="<?php echo $password?>" class="form-control" placeholder=" Atleast 8 characters">
+                       </div>
+                       
+                   </div>
+ 
+                     </form>  
+                    <br/><br/>
+        </div>
+    </div>
 
-
-        <br />
-
-
-    </form>
-
-    <br />
-    <div id="aSubmitted"></div>
-    <br />
-    <br />
-    <?php include("footer.php");?>
+    
+    <!-- Footer -->
+    <footer class="container py-4">
+        <div class="row">
+        <div class="col-12 col-md">
+            <small class="d-block mb-3 text-muted">&copy; 2021-2022 </br> </small>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Team</h5>
+            <ul class="list-unstyled text-small">
+            <li><a class="text-muted">Fatema Akther</a></li>
+            <li><a class="text-muted">Julie Trinh</a></li>
+            <li><a class="text-muted">Jasmit Kalsi</a></li>
+            <li><a class="text-muted">Dzmitry Fiodarau</a></li>
+            <li><a class="text-muted">Alice Chen</a></li>
+            <li><a class="text-muted">Georgia Pitic</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Promotions</h5>
+            <ul class="list-unstyled text-small">
+            <li><a class="text-muted">Newsletter</a></li>
+            <li><a class="text-muted">Gift cards</a></li>
+            <li><a class="text-muted">Contests</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+            <li><a href="Contact Us.html" class="text-muted">Contact us</a></li>
+            <li><a class="text-muted">Our story</a></li>
+            <li><a class="text-muted">FAQ</a></li>
+            </ul>
+        </div>
+        </div>
+    </footer>
+    </body>
+</html>
