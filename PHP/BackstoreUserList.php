d27b5a9f726b2d069abe1147351470eb95bb83eb
fatema-a <78175202+fatema-a@users.noreply.github.com>
2021-04-20 02:56:11 -0400

    Update BackstoreUserList.php

--- PHP/BackstoreUserList.php
+++ PHP/BackstoreUserList.php
@@ -36,7 +36,7 @@
             <p><a href="BackstoreProductList.php">Product List</a></p>
             <p><a href="BackstoreOrderList.php">Order List</a></p>
             <p> <a href="BackstoreUserList.php">User List</a></p>
-            <p style="font-weight:bold;"><a href="../GroceryStore-1.php">Log Out</a></p>
+            <p style="font-weight:bold;"><a href="GroceryStore-1.php">Log Out</a></p>
             </div>
 
 
